350ad149c67041a9cfb73a6861760de082fa6f7c
lugthebug7 <123565713+lugthebug7@users.noreply.github.com>
2024-05-08 15:15:22 -0400

    Profile page now has stylized, functioning navigation of in-progress quests, which are pulled from backend

--- backend/app/api/endpoints/users.py
+++ backend/app/api/endpoints/users.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
+from starlette.responses import PlainTextResponse
+
 from ...database import SessionLocal
 
 from passlib.context import CryptContext
@@ -7,13 +9,74 @@ from pydantic import BaseModel
 from jose import JWTError, jwt
 from datetime import datetime, timedelta
 
+import base64
+
+from pydantic import BaseModel
+from dotenv import load_dotenv
+
+
+from ...models import *
+
+load_dotenv()
 
 router = APIRouter()
 
-@router.post("/getUserProgressQuests")
-def get_user_progress_quests():
-    pass
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
-@router.post("/getUserCompleteQuests")
-def get_user_complete_quests():
-    pass
+class UserRequest(BaseModel):
+    current_username: str
+
+
+def get_progress_quests(db: Session, user_id: int):
+    quests_in_progress = db.query(Quests). \
+        join(QuestsInProgress, Quests.id == QuestsInProgress.quest_id). \
+        join(Users, Users.id == QuestsInProgress.user_id). \
+        filter(QuestsInProgress.user_id == user_id). \
+        all()
+    return quests_in_progress
+
+
+def get_complete_quests(db: Session, user_id:int):
+    quests_completed = db.query(Quests). \
+        join(QuestsCompletedBy, Quests.id == QuestsCompletedBy.quest_id). \
+        join(Users, Users.id == QuestsCompletedBy.user_id). \
+        filter(QuestsCompletedBy.user_id == user_id). \
+        all()
+    return quests_completed
+
+
+@router.post("/getUserQuests")
+async def get_user_quests(user: UserRequest, db: Session = Depends(get_db)):
+    try:
+        current_user = db.query(Users).filter(Users.username == user.current_username).first()
+        if not current_user:
+            raise HTTPException(status_code=404, detail="User not found")
+        user_id = current_user.id
+        progress_quests = get_progress_quests(db, user_id)
+        complete_quests = get_complete_quests(db, user_id)
+
+        progress_quests_data = [format_quest_data(quest) for quest in progress_quests]
+        complete_quests_data = [format_quest_data(quest) for quest in complete_quests]
+
+        return {"complete_quests": complete_quests_data, "progress_quests": progress_quests_data}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+def format_quest_data(quest):
+    return {
+        "id": quest.id,
+        "title": quest.title,
+        "description": quest.description,
+        "image": quest.image if quest.image else None,
+        "objective1": quest.objective1 if quest.objective1 else None,
+        "objective2": quest.objective2 if quest.objective2 else None,
+        "objective3": quest.objective3 if quest.objective3 else None,
+        "objective4": quest.objective4 if quest.objective4 else None,
+        "objective5": quest.objective5 if quest.objective5 else None
+    }
--- src/pages/ProfilePage/ProfilePage.js
+++ src/pages/ProfilePage/ProfilePage.js
@@ -2,21 +2,79 @@ import React, {useState, useEffect} from 'react';
 import HomePageLoggedOut from "../HomePageLoggedOut/HomePageLoggedOut";
 import { useUser } from '../../contexts/UserContext';
 import { useNavigate } from "react-router-dom";
-import NavBar from "../../components/NavBar/NavBar";
-import Carousel from "../../components/Carousel/Carousel";
 import './ProfilePage.css';
 import QuestModal from "../../components/QuestModal/QuestModal";
+import groupQuests from "../../components/Carousel/Carousel";
+import ReactDOM from 'react-dom';
 
 const directory = "http://localhost:5001/uploads/";
 
 
-function displayProgressQuests() {
+function UserQuestContainer({item, index, username}) {
+    const [isHovered, setIsHovered] = useState(false);
+    const [isModalOpen, setIsModalOpen] = useState(false);
 
+    const toggleModal = () => {
+        setIsModalOpen(!isModalOpen);
+    };
+
+    return (
+        <div
+            className="quest-container-special"
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
+            onClick={toggleModal}
+            key={index}
+        >
+            <ssLink to={`/quest/${item.id}`}>
+                <img src={directory + item.image} alt={item.title}/>
+                <div className={`detail-panel ${isHovered ? 'visible' : ''}`}>
+                    <div className="quest-card-title">{item.title}</div>
+                    <div className="quest-card-description">{item.description}</div>
+                </div>
+            </ssLink>
+            {isModalOpen && (
+                <QuestModal show={isModalOpen} onClose={toggleModal} item={item} username={username} fetchStatus={true} />
+            )}
+        </div>
+    );
 }
 
-function displayCompleteQuests() {
 
+function DisplayQuests(theQuests) {
+    const fullGroups = [];
+    let currentGroup = [];
+    const { user } = useUser();
+
+    theQuests.forEach((quest, index) => {
+        currentGroup.push(quest);
+        if ((index + 1) % 3 === 0) {
+            fullGroups.push(currentGroup);
+            currentGroup = [];
+        }
+    });
+
+    if (currentGroup.length > 0) {
+        while (currentGroup.length < 3) {
+            currentGroup.push(...theQuests.slice(0, 3 - currentGroup.length));
+        }
+        fullGroups.push(currentGroup);
+    }
+    console.log(user.username)
+
+    return (
+        <div className="quest-group-container-user">
+            {fullGroups.map((group, index) => (
+                <div key={index} className="quest-group-user">
+                    {group.map((quest, index) => (
+                        <UserQuestContainer item={quest} index={index} username={user.username} />
+                    ))}
+                </div>
+            ))}
+        </div>
+    );
 }
+
 
 function ProfilePage() {
     const [progressQuests, setProgressQuests] = useState([]);
@@ -25,15 +83,20 @@ function ProfilePage() {
     const [activeTab, setActiveTab] = useState('progress');
 
     useEffect(() => {
-        const fetchQuests = async () => {
+        const fetchUserQuests = async () => {
+            if (!user.username) {
+                console.error('Username is undefined or empty.');
+            }
             try {
+                console.log('Sending username:', user.username);
                 const response = await fetch('http://localhost:5001/users/getUserQuests', {
                     method: 'POST',
                     headers: { 'Content-Type': 'application/json' },
-                    body: JSON.stringify({ username: user.username })
+                    body: JSON.stringify({ current_username: user.username })
                 });
                 if (response.ok) {
                     const data = await response.json();
+                    console.log('Data:', data)
                     setCompleteQuests(data.complete_quests);
                     setProgressQuests(data.progress_quests);
                 } else {
@@ -43,28 +106,33 @@ function ProfilePage() {
                 console.error('Error fetching quests:', error);
             }
         };
-        fetchQuests();
+        fetchUserQuests();
     }, [user.username]);
 
     return (
         <div className="profile-page">
             <div className="user-info-container">
             </div>
-
-            <div className="user-quests-container">
-                <div className={`progress-button ${activeTab === 'progress' ? 'active' : ''}`}
-                     onClick={() => setActiveTab('progress')}>
-                    In-Progress
+            <div className="entire-quests-section-container">
+                <div className="complete-progress-controller-container">
+                    <div className={`progress-button ${activeTab === 'progress' ? 'active' : ''}`}
+                         onClick={() => setActiveTab('progress')}>
+                        In-Progress
+                    </div>
+                    <div className={`complete-button ${activeTab === 'completed' ? 'active' : ''}`}
+                         onClick={() => setActiveTab('completed')}>
+                        Completed
+                    </div>
                 </div>
-                <div className={`complete-button ${activeTab === 'completed' ? 'active' : ''}`}
-                     onClick={() => setActiveTab('completed')}>
-                    Completed
-                </div>
-                <div className="quest-content">
+                <div className="user-quests-container">
                     {activeTab === 'progress' ? (
-                        <div>{progressQuests.map(quest => <div key={quest.id}>{quest.name}</div>)}</div>
+                        <div>{progressQuests.map(quest => <div key={quest.id}>{quest.name}</div>)}
+                            {DisplayQuests(progressQuests)}
+                        </div>
                     ) : (
-                        <div>{completeQuests.map(quest => <div key={quest.id}>{quest.name}</div>)}</div>
+                        <div>{completeQuests.map(quest => <div key={quest.id}>{quest.name}</div>)}
+                            {DisplayQuests(completeQuests)}
+                        </div>
                     )}
                 </div>
             </div>
